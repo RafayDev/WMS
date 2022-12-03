c315200c6ae1c8dc1100f77251fb69795a684a4e
RafayDev <imabdulrafayy@gmail.com>
2022-12-03 19:07:50 +0500

    Login Page

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,33 @@
+
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<div class="container"> 
+    <div class="row">
+        <div class="col-md-10">
+        </div>
+        <div class="col-md-2">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/home">Overview</a>
+      </li>
+      <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="false" aria-expanded="false" >
+                     Products
+                </a>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">
+                        Add Products
+                  </a>
+                  <a class="dropdown-item" href="#">
+                        Listed Products
+                  </a>
+               </div>
+        </li>
+    </ul>
+</div>
+    <!-- <div class="col-md-4">
+        </div> -->
+  </div>
+  </div>
+</div>
+</nav>
